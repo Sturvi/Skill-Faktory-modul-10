fabeb77919abf3a41e711bd3d596e2ce746c9b4b
Sturvi <114976816+Sturvi@users.noreply.github.com>
2022-12-20 09:38:45 +0400

    Revert "D5"
    This reverts commit 382dd1f8666912de4c60e36ec68a4955969da660.

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,25 +1,11 @@
 import java.io.*;
-import java.nio.charset.Charset;
+import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-        copyFileUsingStream(new File("src/win1251.txt"),
-                Charset.forName("windows-1251"),
-                new File("src/utf8.txt"),
-                Charset.forName("utf-8"));
-    }
 
-    private static void copyFileUsingStream(String source, String dest) throws IOException {
+        Scanner writer = new Scanner("Test/src/output.txt");
 
-        InputStream fis = new FileInputStream(source);
-        OutputStream fos = new FileOutputStream(dest);
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = fis.read(buffer)) > 0) {
-            fos.write(buffer, 0, length);
-        }
-        fis.close();
-        fos.close();
+        writer.wr
     }
 }
